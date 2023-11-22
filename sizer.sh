c4712e25612e6176f7a6126d5f51cdfe451933c5
Ilya Zhivotikov <48191782+MetalUndivided@users.noreply.github.com>
2023-11-22 23:18:06 +0700

    Add files via upload

--- sizer.sh
+++ sizer.sh
@@ -1,4 +1,11 @@
-du -BG | perl -ne '@l = split(); print "@l[1]\n" if @l[0]>=5' | while read dirname; do
-    #du -d 12 -BG $dirname > $dirname/disk_usage.txt
-    echo $dirname
+reportfile='size_makeup.txt'
+
+du -BG | perl -ne '@l = split(); print "@l[1]\n" if @l[0]>=200' | while read dirname; do
+
+    invocation_date=$(date '+%d.%m.%y %H:%M:%S %:z')
+    echo "This directoy size makeup as of ${invocation_date}:" > $dirname/$reportfile
+    echo '' >> $dirname/$reportfile
+
+    du -d 1 -BG $dirname >> $dirname/$reportfile | sort -r -h
+
 done
